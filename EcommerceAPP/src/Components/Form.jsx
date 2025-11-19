222164da3fd19a888f7c69792ef5426d31adef94
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-11-19 22:49:43 +0800

    commit

--- EcommerceAPP/src/Components/Form.jsx
+++ EcommerceAPP/src/Components/Form.jsx
@@ -2,9 +2,13 @@ function Form(){
     return(
         <>
             <div className="flex 2xl:border-2 h-120 w-full xl:border-2 lg:border-2 md:border-2 sm:border-2 xs:border-2">
-                <form action="" className="m-auto rounded-xl shadow-2xl h-100 w-78 ">
+                <form action="" className=" m-auto rounded-xl shadow-2xl h-100 w-78 ">
+
                     <label htmlFor="" className="font-serif text-base" >Image: </label>
-                    <input type="" />
+                    <input className="font-serif text-base border-1 border-gray-500 bg-gray-200 w-22 rounded-sm"  type="file" accept="image/png, image/jpeg"/>
+
+                    <label className="font-serif text-base"  htmlFor="">Product Name: </label>
+                    <input className="font-serif text-base border-gray-500 bg-gray-200 rounded-sm" type="text" />
                 </form>
             </div>
         </>
