181498d89c434b491f3f471042240b6cba9436d1
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-11-19 22:19:21 +0800

    commit

---
+++ EcommerceAPP/src/Components/Form.jsx
@@ -0,0 +1,14 @@
+function Form(){
+    return(
+        <>
+            <div className="flex 2xl:border-2 h-120 w-full xl:border-2 lg:border-2 md:border-2 sm:border-2 xs:border-2">
+                <form action="" className="m-auto rounded-xl shadow-2xl h-100 w-78 ">
+                    <label htmlFor="" className="font-serif text-base" >Image: </label>
+                    <input type="" />
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default Form
---
+++ EcommerceAPP/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+        <>
+            <div className="h-20 w-full bg-blue-400">
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default Header
